d56442237e561f294ed5527a4bcf9fe3f47590ec
Cameron May <camerondmay@gmail.com>
2019-08-25 21:26:52 +1200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -163,7 +163,7 @@ function wetDrawing(_x, _y, pX, pY) {
     _r = drawLayer.pixels[off];
     _g = drawLayer.pixels[off + 1];
     _b = drawLayer.pixels[off + 2];
-    _a = drawLayer.pixels[off + 3] * 0.1;
+    _a = drawLayer.pixels[off + 3] * 0.5;
 
   drawLayer.stroke(_r, _g, _b, _a);
   drawLayer.strokeWeight(25); // for line work
