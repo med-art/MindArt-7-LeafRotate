0aacb842364790d8896892acbd1f367adc3cc29b
Cameron May <camerondmay@gmail.com>
2019-08-26 22:29:52 +1200

    Update ui.js

--- ui.js
+++ ui.js
@@ -2,6 +2,7 @@ let button1, button2, button3, button4;
 let colH1, colH2, colH3;
 let rectWidth;
 let colChoice = 0;
+let counter = 0;
 
 let brushColours = [239, 51, 64, 168, 199, 0, 255, 215, 0, 241, 230, 178, 0, 107, 56];
 let hexColours = ["#EF3340", "#A8C700", "#FFD700", "#FA6122", "#007236", "#4C6A90"]
@@ -15,8 +16,6 @@ function writeTextUI() {
   textSize(longEdge / 50);
   fill(0);
   noStroke();
-
-
 
   button2 = createButton('Draw');
   button2.class('deselect');
@@ -42,28 +41,88 @@ function writeTextUI() {
 
   if (width > height) {
 
-      rectWidth = height / 6;
+    rectWidth = height / 6;
 
     button2.position(rectWidth / 2, 0);
     button3.position(rectWidth / 2, lmax * 3.5);
     button1.position(rectWidth / 2, lmax * 7);
 
-    button4.position(rectWidth / 2, windowHeight - lmax * 8);
+    button4.position(rectWidth / 2, windowHeight - lmax * 12);
     button5.position(rectWidth / 2, windowHeight - lmax * 4);
-  }
-
-else if (width <= height) {
+  } else if (width <= height) {
 
     rectWidth = width / 6;
-    button2.position(0, windowHeight - rectWidth/2 - (4*lmax));
-    button3.position(12 * lmax, windowHeight -  rectWidth/2- (4*lmax));
-    button1.position(24 * lmax, windowHeight -  rectWidth/2- (4*lmax));
+    button2.position(0, windowHeight - rectWidth / 2 - (4 * lmax));
+    button3.position(12 * lmax, windowHeight - rectWidth / 2 - (4 * lmax));
+    button1.position(24 * lmax, windowHeight - rectWidth / 2 - (4 * lmax));
 
-    button4.position(windowWidth - (28 * lmax) - (lmax * 1.5), windowHeight - rectWidth/2 - (4*lmax));
-    button5.position(windowWidth - (18 * lmax) - (lmax * 1.5), windowHeight - rectWidth/2 - (4*lmax));
+    button4.position(windowWidth - (28 * lmax) - (lmax * 1.5), windowHeight - rectWidth / 2 - (4 * lmax));
+    button5.position(windowWidth - (18 * lmax) - (lmax * 1.5), windowHeight - rectWidth / 2 - (4 * lmax));
   }
 
 }
+
+function writeStageUI() {
+
+
+  textSize(longEdge / 50);
+  fill(0);
+  noStroke();
+
+  button6 = createButton('New leaf');
+  button6.class('restart');
+  button6.mousePressed(chooseLeaf);
+
+  if (width > height) {
+
+    rectWidth = height / 6;
+
+
+    button6.position(rectWidth / 2, windowHeight - lmax * 8);
+
+
+  } else if (width <= height) {
+
+
+    button6.position(windowWidth - (18 * lmax) - (lmax * 1.5), windowHeight - rectWidth / 2 - (8 * lmax));
+  }
+}
+
+function writeScaleUI() {
+
+  textSize(longEdge / 50);
+  fill(0);
+  noStroke();
+
+  button7 = createButton('Scale');
+  button7.class('deselect');
+  button7.mousePressed(scaleLeaf);
+
+  if (width > height) {
+
+    rectWidth = height / 6;
+
+    button7.position((rectWidth / 2), lmax * 10.5);
+
+
+  } else if (width <= height) {
+
+    button7.position(24 * lmax, windowHeight - rectWidth / 2 - (8 * lmax));
+
+  }
+}
+
+
+function chooseLeaf() {
+  leafSelector = int(random(0, 13));
+  touchMoved();
+  rotateImg();
+}
+
+function scaleLeaf() {
+
+}
+
 
 
 function rotateImg() {
@@ -92,8 +151,15 @@ function paintImg() {
 
 
 function restart() {
+  counter++;
+  if (counter === 5) {
+    writeStageUI();
+  }
+  if (counter === 10) {
+    writeScaleUI();
+  }
   drawLayer.clear();
-    drawLayer2.clear();
+  drawLayer2.clear();
   leafSelector = int(random(0, 13));
   rot = 0;
   drawState = 0;
@@ -118,20 +184,20 @@ function makeSwatch() {
     for (let i = 1; i < 7; i++) {
       uiLayer.fill(color(hexColours[i - 1]));
       uiLayer.noStroke();
-          uiLayer.rect(0, (rectWidth * i) - rectWidth, rectWidth / 2, rectWidth * i);
+      uiLayer.rect(0, (rectWidth * i) - rectWidth, rectWidth / 2, rectWidth * i);
       image(uiLayer, 0, 0);
     }
   }
 
-// portrait, UI on bottom
+  // portrait, UI on bottom
   if (width <= height) {
 
-  rectWidth = width / 6;
+    rectWidth = width / 6;
 
     for (let i = 1; i < 7; i++) {
       uiLayer.fill(color(hexColours[i - 1]));
       uiLayer.noStroke();
-      uiLayer.rect(rectWidth*(i-1), height-(rectWidth/2), rectWidth*(i), height);
+      uiLayer.rect(rectWidth * (i - 1), height - (rectWidth / 2), rectWidth * (i), height);
       image(uiLayer, 0, 0);
     }
   }
